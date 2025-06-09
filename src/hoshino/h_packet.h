9c565b8e19166b73147d33363bd57328d228ad4d
entriphy <1747088+entriphy@users.noreply.github.com>
2025-06-09 09:29:44 -0700

    packet

--- src/hoshino/h_packet.h
+++ src/hoshino/h_packet.h
@@ -25,6 +25,42 @@ typedef struct { // 0x88
     /* 0x78 */ u64 tex2[2];
 } hPACKET_DATA;
 
+typedef struct { // 0x10
+    /* 0x0 */ u16 TBP : 14;
+    /* 0x1 */ u32 : 2;
+    /* 0x2 */ u32 TBW : 6;
+    /* 0x2 */ u32 : 2;
+    /* 0x3 */ u32 PSM : 6;
+    /* 0x3 */ u32 : 2;
+    /* 0x4 */ u32 : 32;
+    /* 0x8 */ u32 PX : 11;
+    /* 0x9 */ u32 : 5;
+    /* 0xa */ u32 PY : 11;
+    /* 0xb */ u32 : 5;
+    /* 0xc */ u32 PW : 12;
+    /* 0xd */ u32 : 4;
+    /* 0xe */ u32 PH : 12;
+    /* 0xf */ u32 : 4;
+} hGIM_PHEAD;
+
+typedef struct { // 0x10
+    /* 0x0 */ u16 CBP : 14;
+    /* 0x1 */ u32 : 2;
+    /* 0x2 */ u32 CBW : 6;
+    /* 0x2 */ u32 : 2;
+    /* 0x3 */ u32 CPSM : 6;
+    /* 0x3 */ u32 : 2;
+    /* 0x4 */ u32 : 32;
+    /* 0x8 */ u32 CX : 11;
+    /* 0x9 */ u32 : 5;
+    /* 0xa */ u32 CY : 11;
+    /* 0xb */ u32 : 5;
+    /* 0xc */ u32 CW : 12;
+    /* 0xd */ u32 : 4;
+    /* 0xe */ u32 CH : 12;
+    /* 0xf */ u32 : 4;
+} hGIM_CHEAD;
+
 extern void hDmaSync(sceDmaChan *DmaChan);
 extern void hPkInit(hPACKET_DATA *pk_data);
 extern void hPkReset(u128 *pk_area);
